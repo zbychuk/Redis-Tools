81fe572436fee2ae805e8e5fb62d282a644dfbef
Zbyszek Swirski <zbych@controtex.com>
2021-04-25 15:52:22 +0100

    Add project files.

---
+++ TestRedisSpeed/Options.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+using CommandLine;
+using CommandLine.Text;
+
+namespace TestRedisSpeed
+{
+
+    internal class Options
+    {
+        private int _season;
+
+        [Option('v', "verbose", HelpText = "Print query progress", Default = false)]
+        public bool Verbose { get; set; }
+
+        [Option("debug", HelpText = "Debug data", Default = false)]
+        public bool Debug { get; set; }
+        [Option("host", HelpText = "host ip", Default="172.16.4.4")] // private Ip - change to your own
+        public string Host { get; set; }
+        [Option("port", HelpText = "Port number", Default = 6377)]
+        public int Port { get; set; }
+
+        [Option('n', "number-of-tests", HelpText = "Number of tests", Default = 100)]
+        public int NumberOfTests { get; set; }
+
+        [Option('t', "thread", HelpText = "No of threads", Default = 1)]
+        public int Threads { get; set; }
+
+        [Option('c',"command", HelpText="Command to send", Default = null)]
+        public string Command { get; set; }
+
+        [Option( "db", HelpText = "Database number", Default = null)]
+        public int DbNumber { get; set; }
+    }
+
+    [Verb("bets", HelpText = "Convert bet stats to default")]
+    internal class BetOptions : Options
+    {
+        [Option("id", HelpText = "convert for this id (from A)")]
+        public int Id { get; set; }
+    }
+
+}
---
+++ TestRedisSpeed/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CommandLine;
+using Newtonsoft.Json;
+using StackExchange.Redis;
+
+namespace TestRedisSpeed
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Parser.Default.ParseArguments<Options>(args)
+                .WithParsed(Run);
+        }
+
+        private static void Run(Options options)
+        {
+            var redis = ConnectionMultiplexer.Connect($"{options.Host}:{options.Port}");
+            var db = redis.GetDatabase(options.DbNumber);
+
+            var endPoints = redis.GetEndPoints();
+            var server = redis.GetServer(endPoints[0]);
+            var keys = server.Keys(0, "*");
+            // if (options.Debug)
+            // {
+            //     foreach (var key in keys)
+            //     {
+            //         Console.WriteLine($"key:{key}, ValueTask: {db.StringGet(key)}");
+            //     }
+            // }
+
+            
+            DateTime start = DateTime.Now;
+            var tasks = new List<Task>();
+            for (var i = 0; i < options.Threads; i++)
+            {
+                var id = i;
+                var t = Task.Run(() => RunTest(options, db, id));
+                tasks.Add(t);
+            }
+
+            Task.WaitAll(tasks.ToArray());
+            DateTime end = DateTime.Now;
+            Console.WriteLine($"Whole test time: {(end - start).TotalMilliseconds}");
+
+            if (!Debugger.IsAttached) return;
+
+            Console.WriteLine("Press enter");
+            Console.ReadLine();
+        }
+
+        private static void RunTest(Options options, IDatabase db, int id)
+        {
+            int noOfTests=options.NumberOfTests;
+            DateTime start = DateTime.Now;
+            for (var i = 0; i < noOfTests; i++)
+            {
+                if (options.Command != null)
+                {
+                    var x = db.Execute(options.Command);
+                    if (options.Debug)
+                    {
+                        Console.WriteLine($"{JsonConvert.SerializeObject(x)}");
+                    }
+                }
+                else
+                {
+                    var key = $"value_{id}";
+                    db.StringSet(key, i);
+                    var o = db.StringGet(key).ToString();
+                    if (o != i.ToString()) Console.WriteLine($"Set {i}, received {o}");
+                }
+            }
+
+            DateTime end = DateTime.Now;
+            Console.WriteLine($"Test count: {noOfTests}, Time: {(end - start).TotalMilliseconds}");
+        }
+    }
+}
